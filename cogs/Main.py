d4857dbb99982851cb0e4cba1406d37d626f43f3
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-03-06 13:52:53 -0600

    Update Main.py

--- cogs/Main.py
+++ cogs/Main.py
@@ -1,15 +1,16 @@
 #Required Imports
-import discord
-import random
-import requests
-import http
 from discord.ext import commands, tasks
 from random import choice, choices
 from io import BytesIO
 
+import discord
+import random
+import requests
+import http
+
 footer = "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png"
 
-class Main(commands.Cog): 
+class Main(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -23,7 +24,7 @@ class Main(commands.Cog):
     async def random(self, ctx):
         responses = ['I talk, he talk, why you middle talk?', 'You rotate the ground 4 times..', 'You go and understand the tree.', 'Dont make noise.. principle is rotating in the corridor']
         await ctx.reply(choice(responses))
-    
+
     @commands.command(name='roast')
     async def roast(self, ctx, member:discord.Member = None):
         response = [f'{ctx.author.mention} If laughter is the best medicine, your face must be curing the world.', f'{ctx.author.mention} Youre so ugly that when you tried to enter an ugly contest the judges said, "sorry, no professionals".', f'{ctx.author.mention} Youre so ugly that when you were born they had to put dark tints on your incubator.', f'{ctx.author.mention} Youre so ugly that when you went to the haunted house you came out with a job application.', f'{ctx.author.mention} Youre so ugly, when your mom dropped you off at school she got a fine for littering.', f'{ctx.author.mention} Youre so dumb that when you heard it was chilly outside you ran and got a bowl and spoon.', f'{ctx.author.mention} I am not saying that you are stupid, just that you are constantly unlucky when you try thinking.', f'{ctx.author.mention} Youre so hairy that when you come out of the shower it is like Gorillas In The Mist.', f'{ctx.author.mention} You are so old that when you pass away, there will be a worldwide race between paleontologists to dig you up.']
@@ -81,7 +82,7 @@ class Main(commands.Cog):
             embed=discord.Embed(title="Trinix Rate", description="", color = discord.Color.blurple())
             embed.add_field(name="Results", value=ratea, inline=False)
             embed.set_footer(text="Trinix Made by: Maxim", icon_url = footer)
-            await ctx.reply(embed=embed, mention_author=False) 
-    
+            await ctx.reply(embed=embed, mention_author=False)
+
 def setup(bot): #Must have a setup function
     bot.add_cog(Main(bot)) # Add the class to the cog.
