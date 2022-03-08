6458786b5fba7dc336a8bd41243725891a210468
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-03-08 12:26:10 -0600

    Update listener.py

--- cogs/listener.py
+++ cogs/listener.py
@@ -1,7 +1,9 @@
 #Required Imports
 from discord import message
 from discord.ext import commands, tasks
+
 import discord
+
 
 class listener(commands.Cog):
     def __init__(self, bot):
@@ -10,9 +12,15 @@ class listener(commands.Cog):
     @commands.Cog.listener()#This will tell you is you are missing perms / req
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send('you are missing requirements :rolling_eyes:.')
+            message = "you are missing requirements"
+            embed=discord.Embed(title="Trinix Mod System", description=message, color=0xff0000)
+            await ctx.send(embed=embed)
+
         if isinstance(error, commands.MissingPermissions):
-            await ctx.send("You dont have all the required permissions :angry:")
+            message = "You dont have all the required permissions"
+            message = "you are missing requirements"
+            embed=discord.Embed(title="Trinix Mod System", description=message, color=0xff0000)
+            await ctx.send(embed=embed)
 
 def setup(bot): #Must have a setup function
         bot.add_cog(listener(bot)) # Add the class to the cog.
