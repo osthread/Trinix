a4639a6e612c31414a580a75faf2c5b7c3f92e4a
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-03-20 11:29:42 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ Trinix = commands.Bot(
 )
 
 @Trinix.event
-async def on_ready():#This shows when the bot is online and working
+async def on_ready():
     await client.change_presence(activity=discord.Activity(type = discord.ActivityType.playing, name = "type ,help"))#activity status
     print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
     print('████████ ██████  ██ ███    ██ ██ ██   ██      ██    ███████')
