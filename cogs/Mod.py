860c2d768035b776eef7e290a8c9d7e622972a66
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-03-20 11:41:03 -0500

    Update Mod.py

--- cogs/Mod.py
+++ cogs/Mod.py
@@ -1,6 +1,6 @@
 #Required Imports
-from discord.ext import commands, tasks
 from discord import message
+from discord.ext import commands, tasks
 
 import discord, datetime
 
@@ -9,37 +9,32 @@ class Mod(commands.Cog):
         self.bot = bot
 
     #Admin Commands
-    @commands.command(aliases= ['purge','delete'])
-    @commands.has_permissions(administrator=True)#This makes it where only admins can use this command
-    async def clear(self, ctx, amount=1000):
-        channel = ctx.message.channel
-        messages = []
-        async for message in channel.history(limit=amount + 1):
-                  messages.append(message)
-
-        await channel.delete_messages(messages)
-        message = f'{amount} messages have been purged by {ctx.message.author.mention}'
-        embed=discord.Embed(title="Trinix Mod System", description=message, color=0xff0000)
-        await ctx.send(embed=embed)
+    @commands.command(aliases= ['purge','delete','clean'])
+    @commands.has_permissions(administrator=True)
+    async def clean(self, ctx, limit: int):
+            await ctx.channel.purge(limit=limit)
+            message = f'{limit} messages have been purged by {ctx.message.author.mention}'
+            embed=discord.Embed(title="Trinix Mod System", description=message, color=0x7289da)
+            await ctx.send(embed=embed)
 
     @commands.command()
-    @commands.has_permissions(kick_members = True)#This makes it where only admins can use this command
+    @commands.has_permissions(kick_members = True)# This makes it where only admins can use this command
     async def kick(self, ctx, member: discord.Member, *, why=None):
         await member.kick(reason=why)
-        message = f"**{member} has been kicked from this server by {ctx.author}**"
-        embed=discord.Embed(title="Trinix Mod System", description=message, color=0xff0000)
+        message = f"**{member} has been kicked from this server by {ctx.message.author.mentionr}**"
+        embed=discord.Embed(title="Trinix Mod System", description=message, color=0x7289da)
         await ctx.send(embed=embed)
 
     @commands.command()
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member : discord.Member, *, reason = None):
         await member.ban(reason = reason)
-        message = f'**{member} has been banned from this server by {ctx.author}**'
-        embed=discord.Embed(title="Trinix Mod System", description=message, color=0xff0000)
+        message = f'**{member} has been banned from this server by {ctx.message.author.mention}**'
+        embed=discord.Embed(title="Trinix Mod System", description=message, color=0x7289da)
         await ctx.send(embed=embed)
 
     @commands.command()
-    @commands.has_permissions(administrator = True)#This makes it where only admins can use this command
+    @commands.has_permissions(administrator = True)# This makes it where only admins can use this command
     async def unban(self, ctx, *, member):
         banned_users = await ctx.guild.bans()
         member_name, member_discriminator = member.split("#")
@@ -52,5 +47,13 @@ class Mod(commands.Cog):
                 await ctx.send(f'Unbanned {user.mention}')
                 return
 
-def setup(bot): #Must have a setup function
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def announce(self, ctx, *, message):
+        await ctx.channel.purge(limit=1)
+        embed=discord.Embed(title=f"{ctx.message.guild.name} Announcements", description=message, color=0x7289da)
+        embed.set_footer(text=f"{ctx.message.guild.name}", icon_url = ctx.guild.icon_url)
+        await ctx.send(embed=embed)
+
+def setup(bot): # Must have a setup function
         bot.add_cog(Mod(bot)) # Add the class to the cog.
