2f7efe00afef0a12f0bfb6b5b155ef4430cd2660
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-04-01 11:58:22 -0500

    Update and rename Help.py to help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,8 +1,8 @@
 #Required Imports
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands, tasks, json
 
-class Help(commands.Cog):
+class help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -15,8 +15,8 @@ class Help(commands.Cog):
         embed.add_field(name=":cyclone: **Misc Commands**", value="Command: `.mishelp`")
         embed.add_field(name=":sparkles: **Neko Commands**", value="Command: `.nhelp`")
         embed.add_field(name=":tada: **Fun Commands**", value="Command: `.fhelp`")
-        embed.set_thumbnail(url= "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
-        embed.set_footer(text="Trinix", icon_url = "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
+        embed.set_thumbnail(url= ctx.guild.icon_url)
+        embed.set_footer(text="Trinix", icon_url = ctx.guild.icon_url)
         embed.color = discord.Color.blurple()
         await ctx.reply(embed=embed, mention_author=False)
 
@@ -30,8 +30,8 @@ class Help(commands.Cog):
         embed.add_field(name="**Purging Messages**", value="Command: `.purge <amount>`", inline=False)
         embed.add_field(name="**Announcing**", value="Command: `.announce <message>`", inline=False)
         embed.add_field(name="**Change Server Prefix**", value="Command: `.changeprefix <newprefix>`", inline=False)
-        embed.set_thumbnail(url= "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
-        embed.set_footer(text="Trinix", icon_url = "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
+        embed.set_thumbnail(url= ctx.guild.icon_url)
+        embed.set_footer(text="Trinix", icon_url = ctx.guild.icon_url)
         embed.color = discord.Color.blurple()
         await ctx.reply(embed=embed, mention_author=False)
 
@@ -46,8 +46,13 @@ class Help(commands.Cog):
         embed.add_field(name="**Heads Or Tails**", value="Command: `.coinflip`", inline=False)
         embed.add_field(name="**Rating Members Looks**", value="Command: `.rate <@user/none>`", inline=False)
         embed.add_field(name="**Simp Rating Members**", value="Command: `.simprate <@user/none>`", inline=False)
-        embed.set_thumbnail(url= "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
-        embed.set_footer(text="Trinix", icon_url = "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
+        embed.add_field(name="**Get Truth**", value="Command: `.truth`", inline=False)
+        embed.add_field(name="**Get Dare**", value="Command: `.dare`", inline=False)
+        embed.add_field(name="**Get Would You Ratherr**", value="Command: `.wyr`", inline=False)
+        embed.add_field(name="**Get Never Have I Ever**", value="Command: `.nhie`", inline=False)
+        embed.add_field(name="**Get Paranoia Question**", value="Command: `.nhie`", inline=False)
+        embed.set_thumbnail(url= ctx.guild.icon_url)
+        embed.set_footer(text="Trinix", icon_url = ctx.guild.icon_url)
         embed.color = discord.Color.blurple()
         await ctx.reply(embed=embed, mention_author=False)
 
@@ -61,8 +66,8 @@ class Help(commands.Cog):
         embed.add_field(name="**Search Music**", value="Command: `.search <title>`", inline=False)
         embed.add_field(name="**Pausing Music**", value="Command: `.pause`", inline=False)
         embed.add_field(name="**Unpausing Music**", value="Command: `.resume`", inline=False)
-        embed.set_thumbnail(url= "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
-        embed.set_footer(text="Trinix", icon_url = "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
+        embed.set_thumbnail(url= ctx.guild.icon_url)
+        embed.set_footer(text="Trinix", icon_url = ctx.guild.icon_url)
         embed.color = discord.Color.blurple()
         await ctx.reply(embed=embed, mention_author=False)
 
@@ -76,8 +81,8 @@ class Help(commands.Cog):
         embed.add_field(name="**Trinix Bot Website**", value="Command: `.website`", inline=False)
         embed.add_field(name="Trinix Oauth**", value="Command: `.oauth`", inline=False)
         embed.add_field(name="Password Generator**", value="Command: `.oauth`", inline=False)
-        embed.set_thumbnail(url= "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
-        embed.set_footer(text="Trinix", icon_url = "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
+        embed.set_thumbnail(url= ctx.guild.icon_url)
+        embed.set_footer(text="Trinix", icon_url = ctx.guild.icon_url)
         embed.color = discord.Color.blurple()
         await ctx.reply(embed=embed, mention_author=False)
 
@@ -85,20 +90,21 @@ class Help(commands.Cog):
     async def nhelp(self, ctx, cmd=None):
         embed = discord.Embed()
         embed.title = "Trinix Neko Help / Dashboard"
-        embed.add_field(name="**Hugging Members**", value="Command: `.neko hug <@user/none>`", inline=False)
+        embed.add_field(name="**Hugging Members**", value="Command: `.hug <@user/none>`", inline=False)
+        embed.add_field(name="**Patting Members**", value="Command: `.pat <@user/none>`", inline=False)
+        embed.add_field(name="**Kissing Members**", value="Command: `.kiss <@user/none>`", inline=False)
+        embed.add_field(name="**Slapping Members**", value="Command: `.slap <@user/none>`", inline=False)
+        embed.add_field(name="**Punching Members**", value="Command: `.punch <@user/none>`", inline=False)
+        embed.add_field(name="**Random Nekolewd**", value="Command: `.nekolewd`", inline=False)
+        embed.add_field(name="**Random Kitsune**", value="Command: `.kitsune`", inline=False)
+        embed.add_field(name="**Random Waifu**", value="Command: `.waifu`", inline=False)
         embed.add_field(name="**Random Neko**", value="Command: `.neko`", inline=False)
-        embed.add_field(name="**Random Kitsune**", value="Command: `.neko kitsune`", inline=False)
-        embed.add_field(name="**Patting Members**", value="Command: `.neko pat <@user/none>`", inline=False)
-        embed.add_field(name="**Random Waifu**", value="Command: `.neko waifu`", inline=False)
-        embed.add_field(name="**Self Crying**", value="Command: `.neko cry`", inline=False)
-        embed.add_field(name="**Kissing Members**", value="Command: `.neko pat <@user/none>`", inline=False)
-        embed.add_field(name="**Slapping Members**", value="Command: `.neko slap <@user/none>`", inline=False)
-        embed.add_field(name="**Self Smug**", value="Command: `.neko smug`", inline=False)
-        embed.add_field(name="**Punching Members**", value="Command: `.neko slap <@user/none>`", inline=False)
-        embed.set_thumbnail(url= "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
-        embed.set_footer(text="Trinix", icon_url = "https://cdn.discordapp.com/attachments/888282878973194271/891935435859820574/default.png")
+        embed.add_field(name="**Self Crying**", value="Command: `.cry`", inline=False)
+        embed.add_field(name="**Self Smug**", value="Command: `.smug`", inline=False)
+        embed.set_thumbnail(url= ctx.guild.icon_url)
+        embed.set_footer(text="Trinix", icon_url = ctx.guild.icon_url)
         embed.color = discord.Color.blurple()
         await ctx.reply(embed=embed, mention_author=False)
 
 def setup(bot): #Must have a setup function
-    bot.add_cog(Help(bot)) # Add the class to the cog.
+    bot.add_cog(help(bot)) # Add the class to the cog.
