716c3958280a436d1a7237c66d9d668fa19b2ab5
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-06-15 01:08:54 -0500

    Update nekos.py

--- cogs/nekos.py
+++ cogs/nekos.py
@@ -4,136 +4,110 @@ from random import choice
 
 import discord, random, requests
 
-api = 'https://neko-love.xyz/api/v1/'
-
 class nekos(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def hug(self, ctx, member:discord.Member = None):
-        endpoint = 'hug'
-        r = requests.get(api + endpoint)
-        if (member == ctx.author or member == None):
-            some = f"{ctx.author.mention} Has hugged themself"
-            embed = discord.Embed(title="Trinix", description=some, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-        else:
-            some1 = f"{ctx.author.mention} Has hugged {member.mention}"
-            embed = discord.Embed(title="Trinix", description=some1, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    async def pat(self, ctx, member:discord.Member = None):
-        endpoint = 'pat'
-        r = requests.get(api + endpoint)
-        if (member == ctx.author or member == None):
-            some = f"{ctx.author.mention} Has Patted themself"
-            embed = discord.Embed(title="Trinix", description=some, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-        else:
-            some1 = f"{ctx.author.mention} Has Patted {member.mention}"
-            embed = discord.Embed(title="Trinix", description=some1, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    async def kiss(self, ctx, member:discord.Member = None):
-        endpoint = 'kiss'
-        r = requests.get(api + endpoint)
-        if (member == ctx.author or member == None):
-            some = f"{ctx.author.mention} Has Kissed themself"
-            embed = discord.Embed(title="Trinix", description=some, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-        else:
-            some1 = f"{ctx.author.mention} Has Kissed {member.mention}"
-            embed = discord.Embed(title="Trinix", description=some1, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    async def slap(self, ctx, member:discord.Member = None):
-        endpoint = 'slap'
-        r = requests.get(api + endpoint)
-        if (member == ctx.author or member == None):
-            some = f"{ctx.author.mention} Has Slapped themself"
-            embed = discord.Embed(title="Trinix", description=some, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-        else:
-            some1 = f"{ctx.author.mention} Has Slapped {member.mention}"
-            embed = discord.Embed(title="Trinix", description=some1, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    async def punch(self, ctx, member:discord.Member = None):
-        endpoint = 'punch'
-        r = requests.get(api + endpoint)
-        if (member == ctx.author or member == None):
-            some = f"{ctx.author.mention} Has Punched themself"
-            embed = discord.Embed(title="Trinix", description=some, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-        else:
-            some1 = f"{ctx.author.mention} Has Punched {member.mention}"
-            embed = discord.Embed(title="Trinix", description=some1, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    async def smug(self, ctx, member:discord.Member = None):
-        endpoint = 'smug'
-        r = requests.get(api + endpoint)
-        if (member == ctx.author or member == None):
-            some = f"{ctx.author.mention} Has smugged"
-            embed = discord.Embed(title="Trinix", description=some, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    async def waifu(self, ctx, member:discord.Member = None):
-        endpoint = 'waifu'
-        r = requests.get(api + endpoint)
-        if (member == ctx.author or member == None):
-            some = f"{ctx.author.mention} Has Generated A Waifu!"
-            embed = discord.Embed(title="Trinix", description=some, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    async def kitsune(self, ctx, member:discord.Member = None):
-        endpoint = 'kitsune'
-        r = requests.get(api + endpoint)
-        if (member == ctx.author or member == None):
-            some = f"{ctx.author.mention} Has Generated Kitsune!"
-            embed = discord.Embed(title="Trinix", description=some, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    async def cry(self, ctx, member:discord.Member = None):
-        endpoint = 'cry'
-        r = requests.get(api + endpoint)
-        if (member == ctx.author or member == None):
-            some = f"{ctx.author.mention} is crying..."
-            embed = discord.Embed(title="Trinix", description=some, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
-
-    @commands.command()
-    async def nekolewd(self, ctx, member:discord.Member = None):
-        endpoint = 'nekolewd'
-        r = requests.get(api + endpoint)
-        if (member == ctx.author or member == None):
-            some = f"{ctx.author.mention} Has Generated Nekolewd!"
-            embed = discord.Embed(title="Trinix", description=some, color = discord.Color.blurple())
-            embed.set_image(url=(r.json()["url"]))
-            await ctx.send(embed=embed)
+    async def n(self, ctx, arg, member:discord.Member = None):
+        r = requests.get(f"https://neko-love.xyz/api/v1/{arg}")
+        if arg == "hug":
+            try:
+                if (member == ctx.author or member == None):
+                    embed = discord.Embed(color=0x7289da)
+                    embed.set_author(name=f"{ctx.author.name} Hugged Themself.")
+                    embed.set_image(url=(r.json()["url"]))
+                    await ctx.send(embed=embed)
+                else:
+                    embed = discord.Embed(color=0x7289da)
+                    embed.set_author(name=f"{ctx.author.name} Hugged {member.name}.")
+                    embed.set_image(url=(r.json()["url"]))
+                    await ctx.send(embed=embed)
+            except:
+                print("[NEKO ERROR!]")
+        elif arg == "pat":
+            try:
+                if (member == ctx.author or member == None):
+                    embed = discord.Embed(title=f"{ctx.author.name} Patted Themself.", color=0x7289da)
+                    embed.set_image(url=(r.json()["url"]))
+                    await ctx.send(embed=embed)
+                else:
+                    embed = discord.Embed(title=f"{ctx.author.name} Patted {member.name}.", color=0x7289da)
+                    embed.set_image(url=(r.json()["url"]))
+                    await ctx.send(embed=embed)
+            except:
+                print("[NEKO ERROR!]")
+        elif arg == "kiss":
+            try:
+                if (member == ctx.author or member == None):
+                    embed = discord.Embed(title=f"{ctx.author.name} Kissed Themself", color=0x7289da)
+                    embed.set_image(url=(r.json()["url"]))
+                    await ctx.send(embed=embed)
+                else:
+                    embed = discord.Embed(title=f"Kissed {member.name}", color=0x7289da)
+                    embed.set_image(url=(r.json()["url"]))
+                    await ctx.send(embed=embed)
+            except:
+                print("[NEKO ERROR!]")
+        elif arg == "slap":
+            try:
+                if (member == ctx.author or member == None):
+                    embed = discord.Embed(title=f"{ctx.author.name} Slapped Themself.", color=0x7289da)
+                    embed.set_image(url=(r.json()["url"]))
+                    await ctx.send(embed=embed)
+                else:
+                    embed = discord.Embed(title=f"{ctx.author.name} Slapped {member.name}.", color=0x7289da)
+                    embed.set_image(url=(r.json()["url"]))
+                    await ctx.send(embed=embed)
+            except:
+                print("[NEKO ERROR!]")
+        elif arg == "punch":
+            try:
+                if (member == ctx.author or member == None):
+                    embed = discord.Embed(title=f"{ctx.author.name} Punched Themself.", color=0x7289da)
+                    embed.set_image(url=(r.json()["url"]))
+                    await ctx.send(embed=embed)
+                else:
+                    embed = discord.Embed(title=f"{ctx.author.name} Punched {member.name}.", color=0x7289da)
+                    embed.set_image(url=(r.json()["url"]))
+                    await ctx.send(embed=embed)
+            except:
+                print("[NEKO ERROR!]")
+        elif arg == "smug":
+            try:
+                embed = discord.Embed(title=f"{ctx.author.name} Smugged", color=0x7289da)
+                embed.set_image(url=(r.json()["url"]))
+                await ctx.send(embed=embed)
+            except:
+                print("[NEKO ERROR!]")
+        elif arg == "waifu":
+            try:
+                embed = discord.Embed(title=f"{ctx.author.name} Generated A Waifu!", color=0x7289da)
+                embed.set_image(url=(r.json()["url"]))
+                await ctx.send(embed=embed)
+            except:
+                print("[NEKO ERROR!]")
+        elif arg == "kitsune":
+            try:
+                embed = discord.Embed(title=f"{ctx.author.name} Generated Kitsune!", color=0x7289da)
+                embed.set_image(url=(r.json()["url"]))
+                await ctx.send(embed=embed)
+            except:
+                print("[NEKO ERROR!]")
+        elif arg == "cry":
+            try:
+                embed = discord.Embed(title=f"{ctx.author.name} Is Crying...", color=0x7289da)
+                embed.set_image(url=(r.json()["url"]))
+                await ctx.send(embed=embed)
+            except:
+                print("[NEKO ERROR!]")
+        elif arg == "nekolewd":
+            try:
+                embed = discord.Embed(title=f"{ctx.author.name} Generated Nekolewd!", color=0x7289da)
+                embed.set_image(url=(r.json()["url"]))
+                await ctx.send(embed=embed)
+            except:
+                print("[NEKO ERROR!]")
 
 def setup(bot): #Must have a setup function
     bot.add_cog(nekos(bot)) # Add the class to the cog.
