5136483dedaca43af317b86414a7379bf9058bd0
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-06-15 00:59:08 -0500

    optimizing code

--- trinix.py
+++ trinix.py
@@ -1,41 +1,42 @@
-#Trinix was made by Maxim. Trinix is fully free and will be up 24/7 
-#For anymore info just go to https://trinixbot.xyz
-#If you need to contact me for any reason my discord is lulz#1468
+#Trinix was made by Maxie. Trinix is fully free and will be up 24/7
+#If you need to contact me for any reason my discord is Maxie#0008
 #This is just in case I ever open source this.
 #Required Imports
 from discord.ext import commands
 
-import discord, cogs, json, re, os
+import discord, re, json, os
 
 #Config
 with open("config.json", "r") as confjson:
 	configData = json.load(confjson)
 
-token = configData["Token"]
-prefix = configData["Prefix"]
+def main():
 
-intents = discord.Intents.all()
-intents.members = True
-Trinix = commands.Bot(command_prefix=(prefix), help_command = None, intents = intents)
+    intents = discord.Intents.all()
+    intents.members = True
+    Trinix = commands.Bot(command_prefix=(configData["Prefix"]), help_command = None, intents = intents)
 
-@Trinix.event
-async def on_ready():
-    await Trinix.change_presence(activity=discord.Activity(type = discord.ActivityType.playing, name = "type ,help"))#activity status
-    print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
-    print('████████ ██████  ██ ███    ██ ██ ██   ██      ██    ███████')
-    print('   ██    ██   ██ ██ ████   ██ ██  ██ ██      ███    ██     ')
-    print('   ██    ██████  ██ ██ ██  ██ ██   ███        ██    ███████')
-    print('   ██    ██   ██ ██ ██  ██ ██ ██  ██ ██       ██         ██')
-    print('   ██    ██   ██ ██ ██   ████ ██ ██   ██      ██ ██ ███████')
-    print('   Made by : Maxim Trinix is now up and ready to use!      ')
-    print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
+    @Trinix.event
+    async def on_ready():#This shows when the bot is online and working
+        await Trinix.change_presence(activity=discord.Activity(type = discord.ActivityType.listening, name = "Maxie | .h cmd"))#activity status
+        print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
+        print('████████ ██████  ██ ███    ██ ██ ██   ██      ██    ███████')
+        print('   ██    ██   ██ ██ ████   ██ ██  ██ ██      ███    ██     ')
+        print('   ██    ██████  ██ ██ ██  ██ ██   ███        ██    ███████')
+        print('   ██    ██   ██ ██ ██  ██ ██ ██  ██ ██       ██         ██')
+        print('   ██    ██   ██ ██ ██   ████ ██ ██   ██      ██ ██ ███████')
+        print('   Made by : Maxie Trinix is now up and ready to use!      ')
+        print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
 
-#Cogs
-for f in os.listdir("cogs"):
-    if re.match(r".*\.py.swp", f):
-        pass
-    elif re.match(r".*\.py", f):
-        Trinix.load_extension("cogs." + f.replace(".py", ""))
+    #Cogs
+    for f in os.listdir("cogs"):
+        if re.match(r".*\.py.swp", f):
+            pass
+        elif re.match(r".*\.py", f):
+            Trinix.load_extension("cogs." + f.replace(".py", ""))
 
-#Token
-Trinix.run(token)#you put your bot token inside of config.json
+    #Token
+    Trinix.run(configData["Token"])#you put your bot token inside of config.json
+
+if __name__=="__main__":
+    main()
