f32924661b614fa4b2183fb4c619aadf48cc5ac4
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-06-18 00:13:08 -0500

    Update trinix.py

--- trinix.py
+++ trinix.py
@@ -17,11 +17,6 @@ with open("config.json", "r") as confjson:
 
 def main():
 
-    def get_prefix(client, message): ##first we define get_prefix
-        with open('prefixes.json', 'r') as f: ##we open and read the prefixes.json, assuming it's in the same file
-            prefixes = json.load(f) #load the json as prefixes
-        return prefixes[str(message.guild.id)] #recieve the prefix for the guild id given
-
     intents = discord.Intents.all()
     intents.members = True
     Trinix = commands.Bot(command_prefix=configData["Token"], help_command = None, intents = intents)
