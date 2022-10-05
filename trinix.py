e12f1aa4aa38dadb17c7846f20caa8cc2abc61c4
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-10-04 21:17:07 -0500

    Update trinix.py

--- trinix.py
+++ trinix.py
@@ -1,6 +1,16 @@
-#Trinix was made by Maxim. Trinix is fully free and will be up 24/7
-#If you need to contact me for any reason my discord is Maxie#0008
-#This is just in case I ever open source this.
+# Trinix Rewrite (10/3/2022)
+# This will be fully open-source and will be free to use.
+# If you use Trinix you must give me some credit.
+# If need be make a support ticket on my discord https://discord.gg/eDpPP5BXyf/
+# 
+# Features
+# - Neko Commands
+# - Community Commands
+# - Error listener
+# - Anti Nuke (Updating)
+# - Music Commands (Updating)
+# - Mod Automation (Updating)
+# - Admin Commands (Updating)
 
 # ---------------------------------------------------------------- Required Imports ----------------------------------------------------------------
 
@@ -9,6 +19,7 @@ from discord.ext import commands
 import discord, re, json, os, traceback, sys
 
 # ---------------------------------------------------------------- Config ----------------------------------------------------------------
+
 
 with open("config.json", "r") as confjson:
 	configData = json.load(confjson)
@@ -19,34 +30,32 @@ def main():
 
     intents = discord.Intents.all()
     intents.members = True
-    Trinix = commands.Bot(command_prefix=configData["Token"], help_command = None, intents = intents)
+    Trinix = commands.Bot(command_prefix=configData["Prefix"], help_command = None, intents = intents)# Prefix is inside of config.json
 
     @Trinix.event
-    async def on_ready():#This shows when the bot is online and working
-        await Trinix.change_presence(activity=discord.Activity(type = discord.ActivityType.listening, name = "Maxie | .help cmd"))#activity status
+    async def on_ready():# Will show any errors if any
+        await Trinix.change_presence(activity=discord.Activity(type = discord.ActivityType.listening, name = "Maxie | .help cmd"))# Activity Atatus
         print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
-        print('████████ ██████  ██ ███    ██ ██ ██   ██      ██    ███████')
-        print('   ██    ██   ██ ██ ████   ██ ██  ██ ██      ███    ██     ')
-        print('   ██    ██████  ██ ██ ██  ██ ██   ███        ██    ███████')
-        print('   ██    ██   ██ ██ ██  ██ ██ ██  ██ ██       ██         ██')
-        print('   ██    ██   ██ ██ ██   ████ ██ ██   ██      ██ ██ ███████')
-        print('   Made by : Maxim Trinix is now up and ready to use!      ')
+        print('         ████████ ██████  ██ ███    ██ ██ ██   ██          ')
+        print('            ██    ██   ██ ██ ████   ██ ██  ██ ██           ')
+        print('            ██    ██████  ██ ██ ██  ██ ██   ███            ')
+        print('            ██    ██   ██ ██ ██  ██ ██ ██  ██ ██           ')
+        print('            ██    ██   ██ ██ ██   ████ ██ ██   ██          ')
+        print('            Trinix is now up and ready to use!             ')
         print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
 
-    #Cogs
-    FileNameLst = os.listdir("cogs")
+    FileNameLst = os.listdir("Commands")
     for extension in FileNameLst:
         try:
             if "_" in extension:
                 pass
             else:
-                Trinix.load_extension(f'cogs.{extension[:-3]}')
+                Trinix.load_extension(f'Commands.{extension[:-3]}')
         except Exception as e:
             print(f'Failed to load extension {extension[:-3]}.', file=sys.stderr)
             traceback.print_exc()
 
-    #Token
-    Trinix.run(configData["Token"])#you put your bot token inside of config.json
+    Trinix.run(configData["Token"])# Put your token inside of config.json
 
 if __name__=="__main__":
     main()
