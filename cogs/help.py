0e8632b17222609ab5ebe7d7005e76e50c6f8477
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-10-04 21:21:17 -0500

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -16,7 +16,7 @@ class help(commands.Cog):
             embed = discord.Embed(color=0x7289da)
             embed.title = "[Trinix Help Dashboard]"
             if ctx.message.author.guild_permissions.administrator:
-                embed.add_field(name=":shield: **Admin Commands**", value="Command: `.admin`")
+                embed.add_field(name=":shield: **Admin Commands**", value="Command: `.help admin`")
             embed.add_field(name=":loud_sound: **Music Commands**", value="Command: `.help music`")
             embed.add_field(name=":cyclone: **Misc Commands**", value="Command: `.help misc`")
             embed.add_field(name=":sparkles: **Neko Commands**", value="Command: `.help neko`")
@@ -79,24 +79,23 @@ class help(commands.Cog):
             embed.add_field(name="**Self Smug**", value="Command: `.n smug`")
             embed.set_footer(text="Trinix", icon_url = ctx.guild.icon)
             await ctx.send(embed=embed)
+        elif arg == "admin":
+            if ctx.message.author.guild_permissions.administrator:
+                embed = discord.Embed(color=0x7289da)
+                embed.title = "[Trinix Admin Help Dashboard]"
+                embed.add_field(name="**Kicking Members**", value="Command: `.kick <@user>`")
+                embed.add_field(name="**Banning Members**", value="Command: `.ban <@user> <reason>`")
+                embed.add_field(name="**Unbanning Members**", value="Command: `.unban <userid>`")
+                embed.add_field(name="**Purging Messages**", value="Command: `.clean <amount>`")
+                embed.add_field(name="**Announcing**", value="Command: `.announce <message>`")
+                embed.add_field(name="**Change Bot Prefix**", value="Command: `.changeprefix <newprefix>`")
+                embed.set_footer(text="Trinix", icon_url = ctx.guild.icon)
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send('You arent allowed to use this command.')
         else:
             embed=discord.Embed(title="[Trinix Error System]", description="[ERROR]This isn't a command.", color=0xff0000)
             await ctx.send(embed=embed)
-
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def admin(self, ctx, cmd=None):
-        embed = discord.Embed(color=0x7289da)
-        embed.title = "[Trinix Admin Help Dashboard]"
-        embed.add_field(name="**Kicking Members**", value="Command: `.kick <@user>`")
-        embed.add_field(name="**Banning Members**", value="Command: `.ban <@user> <reason>`")
-        embed.add_field(name="**Unbanning Members**", value="Command: `.unban <userid>`")
-        embed.add_field(name="**Purging Messages**", value="Command: `.clean <amount>`")
-        embed.add_field(name="**Announcing**", value="Command: `.announce <message>`")
-        embed.add_field(name="**Change Bot Prefix**", value="Command: `.changeprefix <newprefix>`")
-        embed.set_footer(text="Trinix", icon_url = ctx.guild.icon)
-        await ctx.send(embed=embed)
 
 def setup(bot): #Must have a setup function
     bot.add_cog(help(bot)) # Add the class to the cog.
