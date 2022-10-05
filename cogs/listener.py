a8481888e42e689a478881d1106318a06dafd7b3
Maxim <76827213+UnknownToska@users.noreply.github.com>
2022-10-04 21:19:05 -0500

    Update listener.py

--- cogs/listener.py
+++ cogs/listener.py
@@ -1,25 +1,27 @@
-#Required Imports
+# ---------------------------------------------------------------- Required Imports ----------------------------------------------------------------
 from discord.ext import commands, tasks
 from discord import message
 
 import discord
 
+# --------------------------------------------------------------------------------------------------------------------------------
+
 class listener(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()#This will tell you is you are missing perms / req
+    @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            message = "you are missing requirements"
-            embed=discord.Embed(title="Trinix Mod System", description=message, color=0xff0000)
+        if isinstance(error, commands.MissingRequiredArgument):# Error Listener
+            embed=discord.Embed(title="[Trinix Error System]", description="[ERROR]You are missing requirements.", color=0xff0000)
             await ctx.send(embed=embed)
 
         if isinstance(error, commands.MissingPermissions):
-            message = "You dont have all the required permissions"
-            message = "you are missing requirements"
-            embed=discord.Embed(title="Trinix Mod System", description=message, color=0xff0000)
+            embed=discord.Embed(title="Trinix Error System", description="[EEROR]You do not have the required permissions.", color=0xff0000)
             await ctx.send(embed=embed)
 
 def setup(bot): #Must have a setup function
         bot.add_cog(listener(bot)) # Add the class to the cog.
+
+# --------------------------------------------------------------------------------------------------------------------------------
+
