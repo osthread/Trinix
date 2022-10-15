6aa0bf8ed9e8d23eb10c4dd2038426c99ca72b67
Maxie <76827213+MaxiesMind@users.noreply.github.com>
2022-10-15 02:48:06 -0500

    Optimized Code
    Please note if the bot isn't kicking or banning any members its probably because the bot is below the members. Move the Trinix role and/ or make a bot role and put it above the members. That should fix everything.

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,8 +1,10 @@
+# Please mind if the bot isnt kicking or banning any members its probably because the bot is below the members. Move Trinix role or make a bot role
+# and put it above the members. 
+
 # ---------------------------------------------------------------- Required Imports ----------------------------------------------------------------
 
 from discord.ext import commands, tasks
-
-import discord, datetime, os, requests
+import discord
 
 # --------------------------------------------------------------------------------------------------------------------------------
 
@@ -10,49 +12,39 @@ class mod(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-# ---------------------------------------------------------------- Admin Commands ----------------------------------------------------------------
-
-    @commands.command()
+    @commands.command(aliases= ['purge','delete','clear'])
     @commands.has_permissions(administrator=True)
     async def clean(self, ctx, limit: int):
-        await ctx.channel.purge(limit=1)
         await ctx.channel.purge(limit=limit)
-        embed=discord.Embed(title="Trinix Mod System", description=f'{ctx.author.mention} Has Purged {limit} Messages!', color=0x7289da)
+        embed = discord.Embed(title="[Mod System] Chat Purge", description=f"{ctx.author.mention} Deleted {limit} messages.", color=0x7289da)
+        embed.set_footer(text=f"{ctx.message.guild.name}", icon_url = ctx.guild.icon)
         await ctx.send(embed=embed)
 
     @commands.command()
     @commands.has_permissions(kick_members = True)
     async def kick(self, ctx, member : discord.Member, reason = None):
-        if reason == None:
-            await ctx.channel.purge(limit=1)
+        await ctx.channel.purge(limit=1)
+        if reason == "None":
             await member.kick(reason = "None")
-            embed=discord.Embed(title="Trinix Mod System", description=f"{member.mention} has been kicked from this server", color=0x7289da)
-            embed.add_field(name="Reason", value=f"{reason}")
-            await ctx.send(embed=embed)
         else:
-            await ctx.channel.purge(limit=1)
             await member.kick(reason=reason)
-            embed=discord.Embed(title="Trinix Mod System", description=f"{member.mention} has been kicked from this server. Reason: {reason}", color=0x7289da)
-            embed.add_field(name="Reason", value=f"{reason}")
-            await ctx.send(embed=embed)
+        embed=discord.Embed(title="Trinix Mod System", description=f"{member.mention} has been kicked from this server by {ctx.author.mention}", color=0x7289da)
+        embed.add_field(name="Reason", value=f"{reason}")
+        await ctx.send(embed=embed)  
 
     @commands.command()
     @commands.has_permissions(ban_members = True)
     async def ban(self, ctx, member : discord.Member, reason = None):
+        await ctx.channel.purge(limit=1)
         if reason == None:
-            await ctx.channel.purge(limit=1)
             await member.ban(reason = "None")
-            embed=discord.Embed(title="Trinix Mod System", description=f"{member.mention} has been Banned from this server", color=0x7289da)
-            embed.add_field(name="Reason", value=f"{reason}")
-            await ctx.send(embed=embed)
         else:
-            await ctx.channel.purge(limit=1)
-            await member.ban(reason=reason)
-            embed=discord.Embed(title="Trinix Mod System", description=f"{member.mention} has been Banned from this server", color=0x7289da)
-            embed.add_field(name="Reason", value=f"{reason}")
-            await ctx.send(embed=embed)
+            await member.ban(reason = "None")
+        embed=discord.Embed(title="[Mod System] Ban Member", description=f"{member.mention} has been Banned from this server by {ctx.author.mention}", color=0x7289da)
+        embed.add_field(name="Reason", value=f"{reason}")
+        await ctx.send(embed=embed)
 
-    @commands.command()#Unban by Panda <3
+    @commands.command()
     @commands.has_permissions(administrator = True)
     async def unban(self, ctx, id): 
         await ctx.channel.purge(limit=1)
@@ -66,12 +58,13 @@ class mod(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def announce(self, ctx, message):
+    async def announce(self, ctx, *, message):
         await ctx.channel.purge(limit=1)
-        embed=discord.Embed(title=f"Announcements", description=message, color=0x7289da)
+        embed=discord.Embed(title=f"{ctx.message.guild.name} Announcement!", description=message, color=0x7289da)
+        embed.set_footer(text=f"{ctx.message.guild.name}", icon_url = ctx.guild.icon)
         await ctx.send(embed=embed)
 
-# --------------------------------------------------------------------------------------------------------------------------------
+def setup(bot): #Must have a setup function
+    bot.add_cog(mod(bot)) # Add the class to the cog.
 
-def setup(bot): # Must have a setup function
-        bot.add_cog(mod(bot)) # Add the class to the cog.
+# --------------------------------------------------------------------------------------------------------------------------------
