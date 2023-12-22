7cfa1e359cbb32154d64a459a20d8fe9e0af1c57
Maxie <76827213+MaxieDev@users.noreply.github.com>
2023-12-22 08:33:20 -0600

    Update trinix.py

--- trinix.py
+++ trinix.py
@@ -1,62 +1,41 @@
-# Trinix Rewrite (10/3/2022)
-# This will be fully open-source and will be free to use.
-# If you use Trinix you must give me some credit.
-# If need be make a support ticket on my discord https://discord.gg/eDpPP5BXyf/
-# 
-# Features
-# - Neko Commands
-# - Community Commands
-# - Error listener
-# - Anti Nuke (Updating)
-# - Music Commands (Updating)
-# - Mod Automation (Updating)
-# - Admin Commands (Updating)
-
-# ---------------------------------------------------------------- Required Imports ----------------------------------------------------------------
-
+from Modules.database.db import DatabaseManager
+from Modules.error.logger import CustomLogger
 from discord.ext import commands
 
-import discord, re, json, os, traceback, sys
+import os, sys, logging, discord, traceback
 
-# ---------------------------------------------------------------- Config ----------------------------------------------------------------
+class Trinix(commands.Bot):
+    def __init__(self):
+        self.db_manager = DatabaseManager()
+        self.custom_logger = CustomLogger('Main', log_level=logging.INFO)
+        self.logger = self.custom_logger.get_logger()
+        self.token = self.get_token()
+        self.bot = self.get_bot()
 
-with open("config.json", "r") as confjson:
-	configData = json.load(confjson)
+    def get_token(self):
+        token = self.db_manager.execute_read_one_query("SELECT token FROM auth;")
+        return token[0]
 
-# ---------------------------------------------------------------- Trinix ----------------------------------------------------------------
+    def get_bot(self):
+        intents = discord.Intents.all()
+        intents.members = True
+        trinix = commands.Bot(command_prefix=".", help_command = None, intents = intents)
+        return trinix
 
-def main():
+    def main(self):
+        FileNameLst = os.listdir("cogs")
+        for extension in FileNameLst:
+            try:
+                if "_" in extension:
+                    pass
+                else:
+                    self.bot.load_extension(f'cogs.{extension[:-3]}')
+            except Exception as e:
+                print(f'Failed to load extension {extension[:-3]}.', file=sys.stderr)
+                traceback.print_exc()
+    
+        self.bot.run(self.token)
 
-    intents = discord.Intents.all()
-    intents.members = True
-    Trinix = commands.Bot(command_prefix=configData["Prefix"], help_command = None, intents = intents)# Prefix is inside of config.json
-
-    @Trinix.event
-    async def on_ready():# Will show any errors if any
-        await Trinix.change_presence(activity=discord.Activity(type = discord.ActivityType.listening, name = "Maxie | .help cmd"))# Activity Atatus
-        print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
-        print('         ████████ ██████  ██ ███    ██ ██ ██   ██          ')
-        print('            ██    ██   ██ ██ ████   ██ ██  ██ ██           ')
-        print('            ██    ██████  ██ ██ ██  ██ ██   ███            ')
-        print('            ██    ██   ██ ██ ██  ██ ██ ██  ██ ██           ')
-        print('            ██    ██   ██ ██ ██   ████ ██ ██   ██          ')
-        print('            Trinix is now up and ready to use!             ')
-        print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
-
-    FileNameLst = os.listdir("cogs")
-    for extension in FileNameLst:
-        try:
-            if "_" in extension:
-                pass
-            else:
-                Trinix.load_extension(f'cogs.{extension[:-3]}')
-        except Exception as e:
-            print(f'Failed to load extension {extension[:-3]}.', file=sys.stderr)
-            traceback.print_exc()
-
-    Trinix.run(configData["Token"])# Put your token inside of config.json
-
-if __name__=="__main__":
-    main()
-
-# --------------------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    run = Trinix()
+    run.main()
