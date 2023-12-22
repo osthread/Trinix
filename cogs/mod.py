151e521a12347682d93d256a42e796e6a516ed43
Maxie <76827213+MaxieDev@users.noreply.github.com>
2023-12-22 08:54:21 -0600

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,6 +1,5 @@
 # Please mind if the bot isnt kicking or banning any members its probably because the bot is below the members. Move Trinix role or make a bot role
 # and put it above the members. 
-
 from discord.ext import commands, tasks
 
 from Modules.database.db import DatabaseManager
@@ -12,7 +11,7 @@ class mod(commands.Cog):
         self.bot = bot
         self.db_manager = DatabaseManager()
 
-    @commands.slash_command(description="Setup log channel")
+    @commands.slash_command(guild_ids=[1091479330502213636, 1187754871336751135], description="Setup log channel")
     @commands.has_permissions(administrator=True)
     async def setup_logger(self, ctx):
         guild = ctx.guild
@@ -26,39 +25,71 @@ class mod(commands.Cog):
             msg = await category.create_text_channel("logger", overwrites=overwrites)
             mg_webhook = await msg.create_webhook(name=f"{msg}_webhook")
             try:
-                self.db_manager.execute_query(f"UPDATE config SET webhook='{mg_webhook.url}' WHERE guild_id = {ctx.guild.id}")
+                self.db_manager.execute_query(f"UPDATE config SET webhook='{mg_webhook.url}' WHERE guild_id = ?", (ctx.guild.id,))
             except:
                 await ctx.respond('Something went wrong.')
  
             await ctx.respond('Log server has been setup.')
 
-    @commands.slash_command(description="Enable logger")
+    @commands.slash_command(guild_ids=[1091479330502213636, 1187754871336751135], description="Enable logger")
     @commands.has_permissions(administrator=True)
-    async def enable(self, ctx, option: Option(str, "Choose a option!", choices=["voice", "message"])): 
+    async def enable(self, ctx, option: discord.Option(str, "Choose a option!", choices=["voice", "message", "join"])): 
+        res = self.db_manager.execute_read_one_query("SELECT voice_logger,message_logger FROM config WHERE guild_id = ?", (ctx.guild.id,))
         if option.lower() == "voice":
-            self.db_manager.execute_query("UPDATE config SET voice_logger = 'on'")
+            if res[0] == "on":
+                return await ctx.respond("Voice Monitor has already been enabled.")
+
+            self.db_manager.execute_query("UPDATE config SET voice_logger = 'on' WHERE guild_id = ?", (ctx.guild.id,))
             embed = discord.Embed(title="[Mod System] Message Logger", description="Voice logger has been enabled!", color=0x00FF00)
             embed.set_footer(text = ctx.guild.name, icon_url = self.bot.user.avatar)
             await ctx.respond(embed=embed)
 
         elif option.lower() == "message":
+            if res[1] == "on":
+                return await ctx.respond("Message Logger has already been enabled.")
+
             self.db_manager.execute_query("UPDATE config SET message_logger = 'on'")
             embed = discord.Embed(title="[Mod System] Message Logger", description="Message logger has been enabled!", color=0x00FF00)
             embed.set_footer(text = ctx.guild.name, icon_url = self.bot.user.avatar)
             await ctx.respond(embed=embed)
 
+        elif option.lower() == "join":
+            if res[1] == "on":
+                return await ctx.respond("Join Detection has already been enabled.")
+
+            self.db_manager.execute_query("UPDATE config SET join_detection = 'on'")
+            embed = discord.Embed(title="[Mod System] Join Detection", description="Join Detection has been enabled!", color=0x00FF00)
+            embed.set_footer(text = ctx.guild.name, icon_url = self.bot.user.avatar)
+            await ctx.respond(embed=embed)
+
     @commands.slash_command(description="Disable logger")
     @commands.has_permissions(administrator=True)
-    async def disable(self, ctx, option: Option(str, "Choose a option!", choices=["voice", "message"])): 
+    async def disable(self, ctx, option: discord.Option(str, "Choose a option!", choices=["voice", "message", "join"])): 
+        res = self.db_manager.execute_read_one_query("SELECT voice_logger,message_logger FROM config WHERE guild_id = ?", (ctx.guild.id,))
         if option.lower() == "voice":
+            if res[0] == "off":
+                return await ctx.respond("Voice Monitor has already been disabled.")
+
             self.db_manager.execute_query("UPDATE config SET voice_logger = 'off'")
             embed = discord.Embed(title="[Mod System] Voice logger", description="Voice logger has been disabled!", color=0x00FF00)
             embed.set_footer(text = ctx.guild.name, icon_url = self.bot.user.avatar)
             await ctx.respond(embed=embed)
 
         elif option.lower() == "message":
+            if res[1] == "off":
+                return await ctx.respond("Message Logger has already been disabled.")
+
             self.db_manager.execute_query("UPDATE config SET message_logger = 'off'")
             embed = discord.Embed(title="[Mod System] Message Logger", description="Message logger has been disabled!", color=0x00FF00)
+            embed.set_footer(text = ctx.guild.name, icon_url = self.bot.user.avatar)
+            await ctx.respond(embed=embed)
+
+        elif option.lower() == "join":
+            if res[1] == "off":
+                return await ctx.respond("Join Detection has already been disabled.")
+
+            self.db_manager.execute_query("UPDATE config SET join_detection = 'off'")
+            embed = discord.Embed(title="[Mod System] Join Detection", description="Join Detection has been disabled!", color=0x00FF00)
             embed.set_footer(text = ctx.guild.name, icon_url = self.bot.user.avatar)
             await ctx.respond(embed=embed)
 
