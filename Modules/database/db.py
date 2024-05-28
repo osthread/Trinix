76c2cedce0cf4bf74abc2e59fcad351a0d0409ef
Maxie <76827213+MaxieDev@users.noreply.github.com>
2024-05-28 12:49:46 -0500

    Update db.py

--- Modules/database/db.py
+++ Modules/database/db.py
@@ -21,10 +21,6 @@ class DatabaseManager:
             self.cur.execute("""CREATE TABLE IF NOT EXISTS auth(id INTEGER PRIMARY KEY,token VARCHAR(255),owner_id VARCHAR(255));""")
             self.cur.execute("""CREATE TABLE IF NOT EXISTS config(id INTEGER PRIMARY KEY,guild_id VARCHAR(255),webhook VARCHAR(255),message_logger VARCHAR(255),voice_logger VARCHAR(255),join_detection VARCHAR(255));""")
             self.cur.execute("""CREATE TABLE IF NOT EXISTS blacklist(id INTEGER PRIMARY KEY,guild_id VARCHAR(255),member VARCHAR(255));""")
-            row = self.cur.fetchone()
-            if row is None:
-                self.cur.execute("""INSERT INTO auth (token,owner_id) VALUES ('Token-Here', 'Owner-ID-HERE');""") # Set both token and owner id here
-            self.conn.commit()
         except sqlite3.Error as e:
             return f"Error setting up the database: {e}"
 
