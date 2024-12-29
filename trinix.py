f0f560ee03d6e7932bb0af393de16cdc06b8bda8
osthread <76827213+osthread@users.noreply.github.com>
2024-12-29 14:49:47 -0600

    Update trinix.py

--- trinix.py
+++ trinix.py
@@ -1,38 +1,37 @@
 from Modules.database.db import DatabaseManager
 from discord.ext import commands
 
-import os, sys, logging, discord, traceback
+import os, discord
 
 class Trinix(commands.Bot):
     def __init__(self):
         self.db_manager = DatabaseManager()
-        self.token = self.get_token()
         self.bot = self.get_bot()
-
-    def get_token(self):
-        token = self.db_manager.execute_read_one_query("SELECT token FROM auth")
-        return token[0]
 
     def get_bot(self):
         intents = discord.Intents.all()
         intents.members = True
-        trinix = commands.Bot(command_prefix=">", help_command = None, intents = intents, owner_id = 1028543893383421954)
+        owner_id = self.db_manager.execute_read_one_query("SELECT owner_id FROM auth")
+        trinix = commands.Bot(command_prefix=">", help_command = None, intents = intents, owner_id = owner_id[0])
         return trinix
 
     def main(self):
+        token = self.db_manager.execute_read_one_query("SELECT token FROM auth")
+
         for extension in os.listdir("cogs"):
             if "_" in extension:
                 pass
             else:
                 self.bot.load_extension(f'cogs.{extension[:-3]}')
     
-        self.bot.run(self.token)
+        self.bot.run(token[0])
 
     def bot_login(self):
         token = self.db_manager.execute_read_one_query("SELECT token FROM auth")
         if token is None:
             bot_token = input("Enter bot token: ")
             owner_id = input("Enter Owner ID: ")
+            self.db_manager.execute_query("INSERT INTO auth (token, owner_id) VALUES (?, ?)", (bot_token, owner_id))
         else:
             self.main()
 
